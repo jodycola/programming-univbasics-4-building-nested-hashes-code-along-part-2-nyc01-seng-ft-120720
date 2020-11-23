13d75b3b1d7e1a0f13e8af23bb13f2f3a7d0d727
Kody Samaroo <kody.samaroo@gmail.com>
2020-11-23 07:21:37 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -22,8 +22,8 @@ def update_status
 
   # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
   # Code your solution here:
-  montague[:hero][status] => "dead"
-  capulet[:heroine][status] => "dead"
+  montague[:hero][status] = "dead"
+  capulet[:heroine][status] = "dead"
 
 
 
